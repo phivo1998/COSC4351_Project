823fd7b4df571f4eb09ce80c55379157dc4d4d69
SinaParvaresh <60529544+SinaParvaresh@users.noreply.github.com>
2021-11-17 20:19:58 -0600

    [WIP]
    revamped the UI. no functionality currently, but the links do work along with the endpoints. reservation endpoint has been made but no content

--- REACT_JS/frontend/src/App.js
+++ REACT_JS/frontend/src/App.js
@@ -2,55 +2,23 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Login from "./components/login";
-import Restaurants from "./components/login";
+import Navbar from "./components/Navbar";
+import SignUp from "./components/pages/SignUp";
+import Login from "./components/pages/login";
+import reservations from "./components/pages/reservation";
 
 function App() {
-  const [user, setUser] = React.useState(null);
-
-  async function login(user = null) {
-    setUser(user);
-  }
-
-  async function logout() {
-    setUser(null);
-  }
-
   return (
-    <div>
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a href="/restaurants" className="navbar-brand">
-          Restaurant page
-        </a>
-        <div className="navbar-nav mr-auto">
-          <li className="nav-item">
-            {user ? (
-              <a onClick={logout} className="nav-link" style={{ cursor: 'pointer' }}>
-                Logout {user.name}
-              </a>
-            ) : (
-              <Link to={"/login"} className="nav-link">
-                Login
-              </Link>
-            )}
-          </li>
-        </div>
-      </nav>
-      <div className="container mt-3">
-        <Router>
-          <Switch>
-            <Route exact path={["/", "/restaurants"]} component={Restaurants} />
-            <Route
-              path="/login"
-              render={(props) => (
-                <Login {...props} login={login} />
-              )}
-            />
-          </Switch>
-        </Router>
-
-      </div>
-    </div>
+    <>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path='/login' component={Login} />
+        <Route path='/sign-up' component={SignUp} />
+        <Route path='/reservations' component={reservations} />
+      </Switch>
+    </Router>
+    </>
   );
 }
 
---
+++ REACT_JS/frontend/src/components/pages/SignUp.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import '../../App.css';
+import './../login.css';
+
+
+export default function SignUp(){
+    
+
+    return (
+        <form className="Login">
+        <div className="form-group row">
+             <label for="inputPassword" className="col-sm-2 col-form-label">First Name</label>
+             <div className="col-sm-10">
+                 <input type="firstName" className="form-control" id="inputFirst" placeholder="First Name" />
+             </div>
+         </div>
+         <div className="form-group row">
+             <label for="inputPassword" className="col-sm-2 col-form-label">Last Name</label>
+             <div className="col-sm-10">
+                 <input type="lastName" className="form-control" id="inputLast" placeholder="Last Name" />
+             </div>
+         </div>
+         <div className='form-group row'>
+             <label for='staticEmail' className="col-sm-2 col-form label">Email</label>
+             <div className="col-sm-10">
+                 <input type="text" className="form-control" placeholder="email@example.com"/>
+             </div>
+         </div>
+         <div className="form-group row">
+             <label for="inputPassword" className="col-sm-2 col-form-label">Password</label>
+             <div className="col-sm-10">
+                 <input type="password" className="form-control" id="inputPassword" placeholder="Password" />
+             </div>
+         </div>
+         <div className="form-group row">
+             <label for="inputPhone" className="col-sm-2 col-form-label">Phone Number</label>
+             <div className="col-sm-10">
+                 <input type="phone" className="form-control" id="inputPhone" placeholder="123-456-7890" />
+             </div>
+         </div>
+         <button className="btn btn-primary" id="signUp">Sign Up</button>
+         
+     </form>
+    )
+}
