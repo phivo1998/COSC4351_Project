d533696b493878ba9b1692ec0a2ab36c6b30fbf1
eliassir <81395157+eliassir@users.noreply.github.com>
2021-11-22 22:24:20 -0600

    Updated Server.js to connect to test database

--- REACT_JS/backend/src/server.js
+++ REACT_JS/backend/src/server.js
@@ -10,21 +10,22 @@ app.use(cors());
 
 const withDB = async (operations) => {
     try {
-        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true});
-        const db = client.db('restaurant');
+        const uri = "mongodb+srv://dev:dev@devcluster.6bqdl.mongodb.net/test?retryWrites=true&w=majority";
+        const client = await MongoClient.connect(uri, { useNewUrlParser: true});
+        const db = client.db('test');
 
         await operations(db);
 
         client.close();
     } catch (error) {
-        // res.status(500).json({message: 'Error connecting to db', error});
+        res.status(500).json({message: 'Error connecting to db', error});
     }
 }
 
 app.post('/api/restaurant', async (req, res) => {
     withDB(async (db) => {
         const body = req.body;
-        const userInfo = await db.collection('authentication').findOne({ username: body.username});
+        const userInfo = await db.collection('users').findOne({ username: body.username});
         console.log(userInfo);
         if(userInfo == null || body.password != userInfo.password) {
             res.status(500).json({ message: 'error' })
