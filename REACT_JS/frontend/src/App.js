ff2afbde4090f4448c972a10b75697c0bf84a09b
eliassir <81395157+eliassir@users.noreply.github.com>
2021-11-29 21:37:59 -0600

    Updated dashboard/removed navbar from dashboard

--- REACT_JS/frontend/src/App.js
+++ REACT_JS/frontend/src/App.js
@@ -10,43 +10,46 @@ import Confirmation from "./components/pages/confirmation";
 import ThankYou from "./components/pages/thankyou";
 
 function App() {
-  let [user,setUser] = React.useState(null)
-  let [userId,setUserId] = React.useState(null)
-  const [admin,setAdmin] = React.useState(null)
-  
-  async function login(user =null,id = null){
+  let [user, setUser] = React.useState(null)
+  let [userId, setUserId] = React.useState(null)
+  const [admin, setAdmin] = React.useState(null)
+
+  async function login(user = null, id = null) {
     console.log(`user set to ${user}`)
 
     setUser(user);
     setUserId(id)
-    
+
   }
 
-  async function logout(){
-    setUser(user=null);
+  async function logout() {
+    setUser(user = null);
   }
 
 
   return (
     <>
-    <Router>
-      <Navbar/>
-      <Switch>
-        
-      <Route path='/login' 
-      render ={(props)=>(
-        <Login {...props} login={login}/>
-      )} />        
-      <Route path='/sign-up' component={SignUp} />
-      <Route path='/confirmation' component={Confirmation} />
-      <Route path='/thankyou' component={ThankYou} />
-      
-      <Route path='/dashboard/:username' 
-      render ={(props)=>(
-        <Dashboard {...props} user={user} username={userId}/>
-      )} />
-      </Switch>
-    </Router>
+      <Router>
+
+        <Switch>
+
+          <Route path='/dashboard/:username'
+            render={(props) => (
+              <Dashboard {...props} user={user} username={userId} />
+            )} />
+          <div>
+            <Navbar />
+            <Route path='/login'
+              render={(props) => (
+                <Login {...props} login={login} />
+              )} />
+            <Route path='/sign-up' component={SignUp} />
+            <Route path='/confirmation' component={Confirmation} />
+            <Route path='/thankyou' component={ThankYou} />
+          </div>
+
+        </Switch>
+      </Router>
     </>
   );
 }
